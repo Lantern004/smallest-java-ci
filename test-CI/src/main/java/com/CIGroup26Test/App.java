65282f5f7da73bfac3853f7cd4f439624e5bbd1c
elisabeth_chen <echen@kth.se>
2022-02-09 13:54:17 +0100

    make maven

---
+++ test-CI/src/main/java/com/CIGroup26Test/App.java
@@ -0,0 +1,13 @@
+package com.CIGroup26Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
